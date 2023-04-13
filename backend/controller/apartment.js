fce983577d6575ba6a25ff5b87ebbd337d08e870
coralie montalbano <montalbanocoralie@gmail.com>
2023-04-13 15:59:51 +0200

    rectif controller

--- backend/controller/apartment.js
+++ backend/controller/apartment.js
@@ -1,34 +1,38 @@
 const apartmentModel = require('../models/Apartment');
 
 exports.getAllApartments = (req, res, next) => {
-  apartmentModel.find().then(
-    (apartments) => {
-      const mappedApartments = apartments.map((apartment) => {
-        apartment.id = req.protocol + '://' + req.get('host') + "/apartments/" + apartment.id;
-        return apartment;
-      });
-      res.status(200).json(mappedApartments);
-    }
+  apartmentModel.find().
+    then(
+    (apartments) =>
+    // {
+    //   const mappedApartments = apartments.map((apartment) => {
+    //     apartment.id = req.protocol + '://' + req.get('host') + "/apartments/" + apartment.id;
+    //     return apartment;
+    //   });
+      res.status(200).json(apartments)
+    // }
   ).catch(
-    () => {
-      res.status(500).send('Database error!');
-    }
-  );
+    (error) => 
+      res.status(400).send({ error }));
+    
+  
 };
 
 exports.getOneApartment = (req, res, next) => {
-  apartmentModel.findById(req.params.id).then(
-    (apartment) => {
-      if (!apartment) {
-        return res.status(404).send('Apartment not found!');
-      }
-      apartment.id = req.protocol + '://' + req.get('host') + '/apartments/'+ apartment.id;
-      res.status(200).json(apartment);
-      console.log(apartment.id);
-    }
-  ).catch(
-    () => {
-      res.status(500).send('Database error!');
-    }
-  )
+  apartmentModel.findById(req.params.id)
+    .then((apartment) => res.status(200).json(apartment))
+  .catch((error) => res.status(404).json({error}))
+  //   (apartment) => {
+  //     if (!apartment) {
+  //       return res.status(404).send('Apartment not found!');
+  //     }
+  //     apartment.id = req.protocol + '://' + req.get('host') + '/apartments/'+ apartment.id;
+  //     res.status(200).json(apartment);
+  //     console.log(apartment.id);
+  //   }
+  // ).catch(
+  //   () => {
+  //     res.status(500).send('Database error!');
+  //   }
+  // )
 };
