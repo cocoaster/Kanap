965b09133cd6ad762f5b0220d8d3167b0018d2f4
coralie montalbano <montalbanocoralie@gmail.com>
2023-04-20 12:46:16 +0200

    READ.me

---
+++ README.md
@@ -0,0 +1,10 @@
+##Procédure de démarrage:
+
+1. Pour démarer le dossier backend :
+- placer le fichier .env à la racine du dossier backend
+- cd backend
+- npm start
+
+2. Pour démarer le dossier frontend:
+- cd frontend
+- npm run dev
--- backend/dist/route/apartment.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var express = require('express');
-var router = express.Router();
-var apartmentCtrl = require('../controller/apartment.js');
-router.get('/', apartmentCtrl.getAllApartments);
-router.get('/:id', apartmentCtrl.getOneApartment);
-module.exports = router;
