169c2f2ded1fbb2577fa762516c249c14523fabb
coralie montalbano <montalbanocoralie@gmail.com>
2023-04-22 19:57:22 +0200

    page erreur

--- frontend/src/pages/Error404.jsx
+++ frontend/src/pages/Error404.jsx
@@ -17,8 +17,13 @@ export default function ErrorPage() {
         <div id="error-page">
         <h1 id="errorNumber">404</h1>
           <p id="errorTxt">Oups! La page que vous demandez n'existe pas.</p>
+          <div id="mobileErrorTxtVersion">
+                      <p className="mobileErrorTxtVersionP pError1">Oups! La page que</p>
+                      <p className="mobileErrorTxtVersionP pError2">vous demandez n'existe pas.</p>
+                    </div>
           <a href="/">
             <p id="homeBack">Retourner sur la page d'accueil</p>
+            
             </a>
         </div>
 
