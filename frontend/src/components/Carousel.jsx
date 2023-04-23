8d0bab499a7161e7f24199ba9a0d3384fd61dc97
coralie montalbano <montalbanocoralie@gmail.com>
2023-04-23 06:27:54 +0200

    correction affichage

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -4,80 +4,87 @@ import { getApartments } from '../service/index.jsx';
 import rightArrow from '../assets/Rightarrow.png';
 import leftArrow from '../assets/Leftarrow.png';
 
-
 function Carousel() {
-const [apartments, setApartments] = useState([]);
-const [slidePosition, setSlidePosition] = useState(0);
-const params = useLocation();
-const apartment = apartments.find((apartment) => apartment.id === params?.state?.id);
-const apartmentPictures = apartment?.pictures || [];
+  // Define states using the useState hook
+  const [apartments, setApartments] = useState([]);
+  const [slidePosition, setSlidePosition] = useState(0);
+  // Get the current URL location
+  const params = useLocation();
+  // Find the apartment with the matching ID from the URL
+  const apartment = apartments.find((apartment) => apartment.id === params?.state?.id);
+  // Get the pictures for the apartment or an empty array if there are none
+  const apartmentPictures = apartment?.pictures || [];
 
-const slideCount = apartmentPictures.length;
-const hasPictures = slideCount > 0;
+  // Calculate the number of slides and whether there are any pictures to display
+  const slideCount = apartmentPictures.length;
+  const hasPictures = slideCount > 0;
 
-useEffect(() => {
-getApartments().then(setApartments);
-}, []);
+  // Fetch the apartment data from the API using the useEffect hook
+  useEffect(() => {
+    getApartments().then(setApartments);
+  }, []);
 
-const handlePrevClick = () => {
-setSlidePosition(slidePosition === 0 ? slideCount - 1 : slidePosition - 1);
-};
+  // Handler functions for clicking the previous and next buttons
+  const handlePrevClick = () => {
+    setSlidePosition(slidePosition === 0 ? slideCount - 1 : slidePosition - 1);
+  };
 
-const handleNextClick = () => {
-setSlidePosition(slidePosition === slideCount - 1 ? 0 : slidePosition + 1);
-};
+  const handleNextClick = () => {
+    setSlidePosition(slidePosition === slideCount - 1 ? 0 : slidePosition + 1);
+  };
 
-return (
-  <div className='carousel-content'>
+  return (
+    <div className='carousel-content'>
       <div className='carousel' id='kasa-carousel'>
-          <div className='carousel-inner'>
-  {hasPictures &&
-  apartmentPictures.map((picture, index) => (
+        <div className='carousel-inner'>
+          {/* Display the apartment pictures as slides */}
+          {hasPictures &&
+            apartmentPictures.map((picture, index) => (
               <div
-    className={`carousel-item ${slidePosition === index ? 'active' : ''}`}
-    key={index}
-  >
-                  <img
-                className='imgCarousel'
-                src={picture}
-                alt={apartment?.title}
+                className={`carousel-item ${slidePosition === index ? 'active' : ''}`}
+                key={index}
+              >
+                <img
+                  className='imgCarousel'
+                  src={picture}
+                  alt={apartment?.title}
+                />
+              </div>
+            ))}
+        </div>
+        {/* Display the previous and next buttons, and the slide counter */}
+        {hasPictures && (
+          <>
+            <button
+              type='button'
+              className='carousel-control-prev'
+              onClick={handlePrevClick}
+            >
+              <img
+                src={leftArrow}
+                alt='Previous'
+                className='carousel-control-icon'
               />
-  </div>
-  ))}
-          </div>
-          {hasPictures && (
-  <>
-          <button
-            type='button'
-            className='carousel-control-prev'
-            onClick={handlePrevClick}
-          >
+            </button>
+            <button
+              type='button'
+              className='carousel-control-next'
+              onClick={handleNextClick}
+            >
               <img
-              src={leftArrow}
-              alt='Previous'
-              className='carousel-control-icon'
-            />
-          </button>
-          <button
-            type='button'
-            className='carousel-control-next'
-            onClick={handleNextClick}
-          >
-              <img
-              src={rightArrow}
-              alt='Next'
-              className='carousel-control-icon'
-            />
-          </button>
-          <div className='carousel-counter'>
-           {slidePosition + 1}/{slideCount}
-          </div>
-
-  </>
-)}
+                src={rightArrow}
+                alt='Next'
+                className='carousel-control-icon'
+              />
+            </button>
+            <div className='carousel-counter'>
+              {slidePosition + 1}/{slideCount}
+            </div>
+          </>
+        )}
       </div>
-  </div>
-);
+    </div>
+  );
 }
 
 export default Carousel;
