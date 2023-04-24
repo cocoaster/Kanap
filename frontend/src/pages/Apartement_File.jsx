b7704fbc1b2b38d6f241f5180bb19f9362fa5c13
coralie montalbano <montalbanocoralie@gmail.com>
2023-04-24 09:03:28 +0200

    retour arriere

--- frontend/src/pages/Apartement_File.jsx
+++ frontend/src/pages/Apartement_File.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import { getApartments } from "../service/index.jsx";
 import AppHeader from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
@@ -6,75 +7,79 @@ import Carousel from "../components/Carousel.jsx";
 import DownArrow from "../assets/DownArrow.png";
 import UpArrow from "../assets/UpArrow.png";
 
-function ApartmentDetail(props) {
-  // Retrieve the id of the selected apartment from the props
-  const { selectedApartmentId } = props;
-  
-  // State to store the list of apartments
-  const [apartments, setApartments] = useState([]);
-  
-  // Find the apartment in the list based on the id
-  const apartment = apartments.find(
-    (apartment) => apartment.id === selectedApartmentId
-  );
 
-  // Fetch the list of apartments from the API when the component mounts
-  useEffect(() => {
-    getApartments().then(setApartments);
-  }, []);
+function ApartmentCard() {
+    // State to store the list of apartments
+    const [apartments, setApartments] = useState([]);
 
-  // State to store the active accordion indexes
-  const [activeAccordionIndexes, setActiveAccordionIndexes] = useState([]);
+    // Retrieve the id of the selected apartment from the location object provided by the router
+    const params = useLocation();
 
-  // Function to toggle the state of an accordion
-  const toggleAccordion = (index) => {
-    if (activeAccordionIndexes.includes(index)) {
-      setActiveAccordionIndexes(
-        activeAccordionIndexes.filter((i) => i !== index)
-      );
-    } else {
-      setActiveAccordionIndexes([...activeAccordionIndexes, index]);
-    }
-  };
-  
-  return (
-    <>
-      <AppHeader />
-      <Carousel
+    // Find the apartment in the list based on the id
+    const apartment = apartments.find(
+        (apartment) => apartment.id === params?.state?.id
+    );
+
+    // Fetch the list of apartments from the API when the component mounts
+    useEffect(() => {
+        getApartments().then(setApartments);
+    }, []);
+   
+
+    // State to store the active accordion indexes
+    const [activeAccordionIndexes, setActiveAccordionIndexes] = useState([]);
+
+    // Function to toggle the state of an accordion
+    const toggleAccordion = (index) => {
+        if (activeAccordionIndexes.includes(index)) {
+            setActiveAccordionIndexes(
+                activeAccordionIndexes.filter((i) => i !== index)
+            );
+        } else {
+            setActiveAccordionIndexes([...activeAccordionIndexes, index]);
+        }
+    };
+    return (
+        <>
+            <AppHeader />
+            <Carousel
         apartments={apartments}
-        selectedApartmentId={selectedApartmentId}
-      />
-      <div className="mediumSection">
-        <div className="leftMediumSection">
-          <h1>{apartment?.title} </h1>
-          <h2 className="subtitleList">{apartment?.location} </h2>
-          <ul className="tagList">
-            {apartment?.tags.map((tag, index) => (
-              <li key={index}>{tag}</li>
-            ))}
-          </ul>
-        </div>
-        <div className="rightMediumSection">
-          <div className="hostContent">
-            <div className="hostName">
-              <p>{apartment?.host.name.split(" ")[0]}</p>
-              <p>{apartment?.host.name.split(" ")[1]}</p>
+        selectedApartmentId={params?.state?.id}
+      />            <div className="mediumSection">
+                <div className="leftMediumSection">
+                    <h1>{apartment?.title} </h1>
+                    <h2 className="subtitleList">{apartment?.location} </h2>
+                    <ul className="tagList">
+                        {apartment?.tags.map((tag, index) => (
+                            <li key={index}>{tag}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="rightMediumSection">
+                    <div className="hostContent">
+                        <div className="hostName">
+                            <p>{apartment?.host.name.split(" ")[0]}</p>
+                            <p>{apartment?.host.name.split(" ")[1]}</p>
+                        </div>
+                        <img
+                            src={apartment?.host.picture}
+                            alt="photo de l'hôte"
+                        />
+                    </div>
+                    <div className="rating">
+                        {Array.from({ length: 5 }, (_, i) => {
+                            const starColor =
+                                i < apartment?.rating ? "#ff6060" : "#e3e3e3";
+                            return (
+                                <span key={i} style={{ color: starColor }}>
+                                    ★
+                                </span>
+                            );
+                        })}
+                    </div>
+                </div>
             </div>
-            <img src={apartment?.host.picture} alt="photo de l'hôte" />
-          </div>
-          <div className="rating">
-            {Array.from({ length: 5 }, (_, i) => {
-              const starColor =
-                i < apartment?.rating ? "#ff6060" : "#e3e3e3";
-              return (
-                <span key={i} style={{ color: starColor }}>
-                  ★
-                </span>
-              );
-            })}
-          </div>
-        </div>
-      </div>
+
             <div className="bottom">
                 <div className="accordionApart-item" id="apartAccordion1">
                     <h3 className="accordion-header">
